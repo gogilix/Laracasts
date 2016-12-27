5cc8fafb8ac08cef75766a98fbb8e1542c527a93
Milan Latinović <Milan Latinovi>
2016-12-27 13:36:57 +0100

    Step 5 - Separate PHP logic from Presentation Completed

--- 1. The PHP Practitioner/index.php
+++ 1. The PHP Practitioner/index.php
@@ -1,42 +1,5 @@
-<!DOCTYPE html>
+<?php
 
-<html lang="en">
+$greeting = 'Hello, World';
 
-	<meta charset = "UTF-8">
-
-	<title>Document </title>
-
-	<style>
-
-		header {
-
-			background: #e3e3e3;
-			padding: 2em;
-			text-align: center;
-		}
-
-	</style>
-
-	<body>
-
-		<header>
-
-			<h1>
-			<?php
-
-				$name = htmlspecialchars($_GET['name']); 
-
-				$greeting = 'Hello World';
-
-				echo 'Hello ' . $name; 
-
-			?>
-
-			<?= "Hello again" ?>
-			</h1>
-
-		</header>
-
-	</body>
-
-</html>
+require 'index.view.php';
---
+++ 1. The PHP Practitioner/index.view.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+	<meta charset = "UTF-8">
+
+	<title>Document </title>
+
+	<style>
+
+		header {
+
+			background: #e3e3e3;
+			padding: 2em;
+			text-align: center;
+		}
+
+	</style>
+
+	<body>
+
+		<header>
+
+			<h1>
+			<?= $greeting; ?>
+			</h1>
+
+		</header>
+
+	</body>
+
+</html>
